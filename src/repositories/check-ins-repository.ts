6b02effc2a749fe4538b12d5a8bc12ef41edf2c6
Felipe Augusto <felipe_spinassi@hotmail.com>
2024-09-09 23:47:40 -0300

    fix conflicts between different S.O

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import { CheckIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
   create: (data: Prisma.CheckInUncheckedCreateInput) => Promise<CheckIn>;
   findByUserIdOnDate: (userId: string, date: Date) => Promise<CheckIn | null>;
 }
-=======
-import { CheckIn, Prisma } from "@prisma/client";
-
-export interface CheckInsRepository {
-  create: (data: Prisma.CheckInUncheckedCreateInput) => Promise<CheckIn>;
-}
->>>>>>> 864bce470c8a3cffd6a56743eb7d87d2ad031279
